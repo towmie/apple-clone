5ed05814810cffc6847b2a7a6b5966ef1dd4bb99
towmie <andre00toz@gmail.com>
2024-06-24 16:05:44 +0300

    feat: add topnav

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-col flex-center">
+        <p className="hero-title">Iphone 15 pro</p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
