86686a7dc81fadf2663e016b234ae86287147bb7
towmie <andre00toz@gmail.com>
2024-06-24 16:42:23 +0300

    feat: add highlights header

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,43 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+
 function Highlights() {
-  return <div>Highlights</div>;
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
+
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.5,
+    });
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-center justify-between">
+          <h1 id="title" className="section-heading">
+            Get the Highlights
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film <img className="ml-2" src={watchImg} />
+            </p>
+            <p className="link">
+              Watch the event <img className="ml-2" src={rightImg} />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Highlights;
