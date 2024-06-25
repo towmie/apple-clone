58ce024d4f0dcc72d16fc15883e9479be4a9a6fb
towmie <andre00toz@gmail.com>
2024-06-25 10:10:29 +0300

    feat: add model swap

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTl } from "../utils/animations";
 
 function Model() {
   const [size, setSize] = useState("small");
@@ -15,15 +16,30 @@ function Model() {
     color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
     img: yellowImg,
   });
-
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
   const cameraControlSmall = useRef();
   const cameraControlLarge = useRef();
 
   const small = useRef(new THREE.Group());
   const large = useRef(new THREE.Group());
 
-  const [smallRotation, setSmallRotation] = useState(0);
-  const [largeRotation, setLargeRotation] = useState(0);
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "small") {
+      animateWithGsapTl(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+    if (size === "large") {
+      animateWithGsapTl(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+  }, [size]);
 
   useGSAP(() => {
     gsap.to("#heading", {
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,49 +1,22 @@
 import gsap from "gsap";
 
 import { ScrollTrigger } from "gsap/all";
+import { rotate } from "three/examples/jsm/nodes/Nodes.js";
 gsap.registerPlugin(ScrollTrigger);
 
-export const animateWithGsap = (target, animationProps, scrollProps) => {
-  gsap.to(target, {
-    ...animationProps,
-    scrollTrigger: {
-      trigger: target,
-      toggleActions: "restart reverse restart reverse",
-      start: "top 85%",
-      ...scrollProps,
-    },
-  });
-};
-
-export const animateWithGsapTimeline = (
-  timeline,
+export const animateWithGsapTl = (
+  tl,
   rotationRef,
   rotationState,
   firstTarget,
   secondTarget,
   animationProps
 ) => {
-  timeline.to(rotationRef.current.rotation, {
+  tl.to(rotationRef.current.rotation, {
     y: rotationState,
     duration: 1,
     ease: "power2.inOut",
   });
-
-  timeline.to(
-    firstTarget,
-    {
-      ...animationProps,
-      ease: "power2.inOut",
-    },
-    "<"
-  );
-
-  timeline.to(
-    secondTarget,
-    {
-      ...animationProps,
-      ease: "power2.inOut",
-    },
-    "<"
-  );
+  tl.to(firstTarget, { ...animationProps, ease: "power2.inOut" }, "<");
+  tl.to(secondTarget, { ...animationProps, ease: "power2.inOut" }, "<");
 };
